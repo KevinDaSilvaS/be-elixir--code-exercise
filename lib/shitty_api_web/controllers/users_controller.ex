2d04ad92479d6b5943a419259a4c2e290e9db52e
Kevin <kevin.kds80@gmail.com>
2024-08-01 00:12:46 +0000

    add controller implementation

--- lib/shitty_api_web/controllers/users_controller.ex
+++ lib/shitty_api_web/controllers/users_controller.ex
@@ -2,7 +2,7 @@ defmodule ShittyApiWeb.UsersController do
   use ShittyApiWeb, :controller
 
   def fetch_users(conn, params) do
-
-    render(conn, :home, layout: false)
+    search_opts = Users.Validator.parse(params)
+    conn |> put_status(200) |> json(UserService.fetch_users(search_opts))
   end
 end
