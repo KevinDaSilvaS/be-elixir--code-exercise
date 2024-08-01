5224caa7c8c833fd54656bfd9befb46d91b5115d
Kevin <kevin.kds80@gmail.com>
2024-08-01 22:52:03 +0000

    add mailer broadcaster

--- lib/shitty_api_web/controllers/users_controller.ex
+++ lib/shitty_api_web/controllers/users_controller.ex
@@ -5,4 +5,9 @@ defmodule SampleApiWeb.UsersController do
     search_opts = Users.Validator.parse(params)
     conn |> put_status(200) |> json(UserService.fetch_users(search_opts))
   end
+
+  def mail_active_users(conn, _params) do
+    Phoenix.PubSub.broadcast(:mailer_pubsub, "request", %{offset: 0, limit: 10})
+    conn |> put_status(200) |> json(%{ok: "Mail request sent successfully"})
+  end
 end
